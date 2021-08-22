b05e285e62c6c21e063c1dfe3716f26367eaadf0
David Echeverri <david.echeverri@timba.co>
2021-08-22 13:55:44 -0500

    implement player move

---
+++ Assets/_Scripts/Deco/MBCharacterStateJetpack.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Madbricks
+{
+    public class MBCharacterStateJetpack : MBCharacterStateBase
+    {
+        private float currentWaitTime;
+        public MBCharacterStateJetpack(MBCharacter character) : base(character)
+        {
+            character.rigidbody2D.gravityScale = 0;
+        }
+
+        public override void ProcessInput(Vector2 direction, bool specialPressed)
+        {
+            character.transform.position += new Vector3(direction.x,direction.y,0) * Time.deltaTime *character.VelocityMove;
+        }
+
+        public override void UpdateState()
+        {
+            currentWaitTime += Time.deltaTime;
+            if(currentWaitTime >= character.JetpackDuration){
+                character.SetState(new MBCharacterStateGrounded(character));
+            }
+        }
+    }
+}
